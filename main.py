be4018b507be16ff2e867cfac36c4de1d03afe26
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-03-26 11:09:35 +1300

    Finished welcome message and added the lists

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import os
 
 os.system("clear")
 
+user = []
+pizza_base = []
+pizza_topings = []
 
 
 def menu():
-    print("Welcome to queenstowns pizzaria")
+    print("Welcome to Queenstowns Pizzaria,\n Please enter your name and select what toppings you want on your pizza! ")
