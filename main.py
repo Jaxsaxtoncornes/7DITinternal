e234e3cdf9a9d5898b232022da5e42a5d5488ac0
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-04-04 13:00:01 +1300

    Added the inputs to the start of ordering sides and the cart, finished exiting the code/simulating hanging up or closing the website

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pizza_base = [["Thin Base", 5],["Plain Base", 6], ["Gluten Free Base", 6.5], ["I
 pizza_crust = [["Plain Crust", 0],["Tomato Crust", 1],["Cheesey Crust", 1],["Vegan Cheesey Crust", 1.5]]
 pizza_cheese = [["Mozzarella", 0.5],["Chedder", 0.5],["4 Cheese's", 1],["Vegan Cheese", 1]]
 pizza_toppings = [["Basil", 0.5],["Garlic", 0.75],["Onions", 1],["Tomatoes", 1.25],["Mushrooms", 1.5],["Green Peppers", 1.75],["Ham", 2],["Pepperoni", 2.5]]
-sides = [["Coca Cola", 2],["Sprite", 2],["Fanta", 2],["Fries", 3],["Onion Rings", 3.5],["Garlic Bread", 4]]
+pizza_sides = [["Coca Cola", 2],["Sprite", 2],["Fanta", 2],["Fries", 3],["Onion Rings", 3.5],["Garlic Bread", 4]]
 user_order = []
 
 def menu():
@@ -19,25 +19,54 @@ def menu():
 def make_pizza():
     os.system("clear")
     print("What type of pizza base would you like")
-    counter = 0
+    counter_1 = 0
     for base in pizza_base:
-        counter = counter + 1
-        print(str(counter) + ". " + str(base[0]) + " $" + str(base[1]))
+        counter_1 = counter_1 + 1
+        print(str(counter_1) + ". " + str(base[0]) + " $" + str(base[1]))
     pizza_base_input = int(input("Enter the number next to the pizza base: "))
     if pizza_base_input == 1:
         user_order.append()
 
+    
 def purchase_sides():
-    print("Placeholder")
-    input("")
+    os.system("clear")
+    print("What side would you like to purchase")
+    counter_5 = 0
+    for sides in pizza_sides:
+        counter_5 = counter_5 + 1
+        print(str(counter_5) + ". " + str(sides[0]) + " $" + str(sides[1]))
+    sides_choice = int(input("What side would you like to purchase?"))
+    if sides_choice == 1:
+        sides.append()
+
 
 def cart():
-    print("Placeholder")
-    input("")
+    os.system("clear")
+    print(user_order)
+    user_input = input("Would you like to check out?").lower()
+    if user_input == "Yes":
+        input("What size pizza would you like?")
+    elif user_input == "No":
+        print("Alright, back to the menu!")
+        time.sleep(1)
+        menu()
+    else:
+        print("Please enter a valid input")
+        time.sleep(1)
+        cart()
 
 def leave():
-    print("Placeholder")
-    input("")
+    os.system("clear")
+    leave_call = input("Are you sure you want to end your order?").lower()
+    if leave_call == "Yes":
+        print("have a good day!")
+    elif leave_call == "No":
+        print("Alright, back to the menu!")
+    else:
+        print("Please enter a valid input")
+        time.sleep(1)
+        leave()
+    
 
 while True:
     try:
