a082da0fb1de97110d832381fb809727b4bd9a74
Jaxsaxtoncornes <jaxsaxtoncornes@gmail.com>
2025-04-04 13:05:36 +1300

    spaced out evenly

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os     #imported os for the os.system("clear")
 import time   #imported time to allow user's to read dialogue before it disapears
 
+
 pizza_base = [["Thin Base", 5],["Plain Base", 6], ["Gluten Free Base", 6.5], ["Italian Herb Base", 7]]
 pizza_crust = [["Plain Crust", 0],["Tomato Crust", 1],["Cheesey Crust", 1],["Vegan Cheesey Crust", 1.5]]
 pizza_cheese = [["Mozzarella", 0.5],["Chedder", 0.5],["4 Cheese's", 1],["Vegan Cheese", 1]]
 pizza_toppings = [["Basil", 0.5],["Garlic", 0.75],["Onions", 1],["Tomatoes", 1.25],["Mushrooms", 1.5],["Green Peppers", 1.75],["Ham", 2],["Pepperoni", 2.5]]
 pizza_sides = [["Coca Cola", 2],["Sprite", 2],["Fanta", 2],["Fries", 3],["Onion Rings", 3.5],["Garlic Bread", 4]]
 user_order = []
+
 
 def menu():
     os.system("clear")
@@ -15,6 +17,7 @@ def menu():
     print("2. Purchase sides")
     print("3. Cart")
     print("4. Leave\n")
+
 
 def make_pizza():
     os.system("clear")
@@ -54,6 +57,7 @@ def cart():
         print("Please enter a valid input")
         time.sleep(1)
         cart()
+
 
 def leave():
     os.system("clear")
